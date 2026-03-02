d94cf022fc79526a12f70a0b19b02f97d907327f
Merijn Brons <merinos.brons@gmail.com>
2026-03-02 19:09:03 +0100

    SSE berekend

--- assignment1/constant.py
+++ assignment1/constant.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 from numpy.typing import NDArray
 from sklearn.linear_model import LinearRegression
@@ -13,10 +14,17 @@ def constant_velocity(positions: NDArray[np.float64]) -> list[float]:
     model.fit(time, positions)
 
     velocity = model.coef_[:, 0]
+    
+    errors = positions - model.predict(time)
+    sse = np.sum(errors**2)
+    print(f"Sum of squared errors: {sse:.4f}")
+    
     return velocity.tolist()
+    
 
 
-def constant_accelaration(positions: NDArray[np.float64]) -> list[float]:
+
+def constant_acceleration(positions: NDArray[np.float64]) -> list[float]:
     """Fits p(t)=b+v*t+0.5*a*t^2 and returns acceleration [ax, ay, az]."""
 
     n_samples = positions.shape[0]
@@ -27,4 +35,26 @@ def constant_accelaration(positions: NDArray[np.float64]) -> list[float]:
     model.fit(x, positions)
 
     acceleration = model.coef_[:, 1]
+  
+    errors = positions - model.predict(x)
+    sse = np.sum(errors**2)
+    print(f"Sum of squared errors: {sse:.4f}")
+    
     return acceleration.tolist()
+
+if __name__ == "__main__":
+
+    positions = np.array([
+        [2.0, 0.0, 1.0],
+        [1.08, 1.68, 2.38],
+        [-0.83, 1.82, 2.49],
+        [-1.97, 0.28, 2.15],
+        [-1.31, -1.51, 2.59],
+        [0.57, -1.91, 4.32]
+    ], dtype=np.float64)
+
+    v = constant_velocity(positions)
+    a = constant_acceleration(positions)
+
+    print("Velocity:", v)
+    print("Acceleration:", a)
