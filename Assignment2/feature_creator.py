0ea3e3c2c33a4e7e713a3a3f9afee0715c0e5027
Maarten Hulsman <maarten.hulsman@outlook.com>
2026-03-14 09:51:34 +0100

    Clear comments so that we can create the features easily

--- Assignment2/feature_creator.py
+++ Assignment2/feature_creator.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 
@@ -6,31 +5,42 @@ def get_width_depth_height(points):
     dims = np.ptp(points, axis=0)
     return dims[0], dims[1], dims[2]
 
-# nog 5 features bouwen
+# NOTE: Geef je feature een passende naam
+# Stijn
+def feature1():
+    ...
+
+def feature2():
+    ...
+
+# Merijn
+def feature3():
+    ...
+
+def feature4():
+    ...
+
+# Maarten
+def feature5():
+    ...
+
+def feature6():
+    ...
+
 
 def load_features(objects: list[np.ndarray]) -> None:
-    """
-    Exports own own_data.txt
-    """
-    for points in objects:
-        width, depth, height = get_width_depth_height(points)
-
-    print("iets")
-
-def load_features(objects: list[np.ndarray]) -> None:
-    """
-    Exporteert de features naar own_data.txt
-    """
     all_rows = []
 
     for i, points in enumerate(objects):
         width, depth, height = get_width_depth_height(points)
+        # NOTE: Hier kan je dus je twee eigen features inladen
         
         row = [i, width, depth, height]
         all_rows.append(row)
 
     output_matrix = np.array(all_rows).astype(np.float32)
 
+    # NOTE: vergeet 'm niet aan de header toe te voegen
     header = 'ID,label,width,depth,height'
     np.savetxt('own_data.txt', output_matrix, fmt='%10.5f', delimiter=',', header=header)
     
