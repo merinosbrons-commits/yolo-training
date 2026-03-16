c9459c4b6a53872ce109edb4f97ef1bde49be296
Merijn Brons <merinos.brons@gmail.com>
2026-03-16 15:10:33 +0100

    feature 3 toegevoegd

--- Assignment2/feature_creator.py
+++ Assignment2/feature_creator.py
@@ -14,8 +14,14 @@ def feature2():
     ...
 
 # Merijn
-def feature3():
-    ...
+def feature3(points):
+# bounding box elongation, de verhouding tussen de langste en kortste zijde van de bounding box
+    x_range = np.max(points[:,0]) - np.min(points[:,0])
+    y_range = np.max(points[:,1]) - np.min(points[:,1])
+
+    elongation = max(x_range, y_range) / (min(x_range, y_range) + 1e-5)
+
+    return elongation
 
 def feature4():
     ...
@@ -34,14 +40,14 @@ def load_features(objects: list[np.ndarray]) -> None:
     for i, points in enumerate(objects):
         width, depth, height = get_width_depth_height(points)
         # NOTE: Hier kan je dus je twee eigen features inladen
-        
-        row = [i, width, depth, height]
+        elongation = feature3(points)
+        row = [i, width, depth, height, elongation]
         all_rows.append(row)
 
     output_matrix = np.array(all_rows).astype(np.float32)
 
     # NOTE: vergeet 'm niet aan de header toe te voegen
-    header = 'ID,label,width,depth,height'
+    header = 'ID,label,width,depth,height,elongation'
     np.savetxt('own_data.txt', output_matrix, fmt='%10.5f', delimiter=',', header=header)
     
     print("Features succesvol opgeslagen in own_data.txt")
