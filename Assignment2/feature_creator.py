dd92fea2c8fc49e5ed8815f3cc572f84f46a330a
Merijn Brons <merinos.brons@gmail.com>
2026-03-17 12:57:50 +0100

    Feature 2 created and data added to own_data.txt

--- Assignment2/feature_creator.py
+++ Assignment2/feature_creator.py
@@ -14,7 +14,7 @@ def feature2():
     ...
 
 # Merijn
-def feature3(points):
+def elongation(points):
 # bounding box elongation, de verhouding tussen de langste en kortste zijde van de bounding box
     x_range = np.max(points[:,0]) - np.min(points[:,0])
     y_range = np.max(points[:,1]) - np.min(points[:,1])
@@ -23,8 +23,20 @@ def feature3(points):
 
     return elongation
 
-def feature4():
-    ...
+def density(points):
+
+    n_points = len(points)
+
+    x_range = np.max(points[:,0]) - np.min(points[:,0])
+    y_range = np.max(points[:,1]) - np.min(points[:,1])
+    z_range = np.max(points[:,2]) - np.min(points[:,2])
+
+   
+    volume = x_range * y_range * z_range
+
+    density = n_points / (volume + 1e-5)
+
+    return density
 
 # Maarten
 def feature5():
@@ -40,14 +52,15 @@ def load_features(objects: list[np.ndarray]) -> None:
     for i, points in enumerate(objects):
         width, depth, height = get_width_depth_height(points)
         # NOTE: Hier kan je dus je twee eigen features inladen
-        elongation = feature3(points)
-        row = [i, width, depth, height, elongation]
+        elongation_laden = elongation(points)
+        density_laden= density(points)
+        row = [i, width, depth, height, elongation_laden, density_laden]
         all_rows.append(row)
 
     output_matrix = np.array(all_rows).astype(np.float32)
 
     # NOTE: vergeet 'm niet aan de header toe te voegen
-    header = 'ID,label,width,depth,height,elongation'
+    header = 'ID,label,width,depth,height,elongation,density'
     np.savetxt('own_data.txt', output_matrix, fmt='%10.5f', delimiter=',', header=header)
     
     print("Features succesvol opgeslagen in own_data.txt")
